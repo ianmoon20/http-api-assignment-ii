87eb3c44812659bbc59fe4015406c320495c43c8
Ian Moon <imm3350@g.rit.edu>
2018-09-24 21:02:29 -0400

    http-assignment-ii solution

---
+++ src/jsonResponses.js
@@ -0,0 +1,93 @@
+// List of users
+const users = {};
+
+const respond = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  // Sending back a list of users
+  const responseJSON = {
+    users,
+  };
+
+  // returning a 200
+  respond(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => {
+  respondMeta(request, response, 200);
+};
+
+const addUser = (request, response, body) => {
+  // Assume fail case
+  const responseJSON = {
+    message: 'Need name and age.',
+  };
+
+  // If either are missing send back a 400 error
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respond(request, response, 400, responseJSON);
+  }
+
+  // Assume we have a successful creation
+  let responseCode = 201;
+
+  // If we already have that user...
+  if (users[body.name]) {
+    // We're updating (204 code)
+    responseCode = 204;
+  } else {
+    // Create a new user
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Successfully created user.';
+    return respond(request, response, responseCode, responseJSON);
+  }
+
+  // We can't send back data with a 204
+  return respondMeta(request, response, responseCode);
+};
+
+const notFound = (request, response) => {
+  // Creating and returning an error message
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  respond(request, response, 404, responseJSON);
+};
+const notFoundMeta = (request, response) => {
+  // No error 4 u
+  respond(request, response, 404);
+};
+
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  addUser,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/getUsers': jsonHandler.getUsers,
+  '/addUser': jsonHandler.addUser,
+};
+
+const onRequest = (request, response) => {
+  const parsedURL = url.parse(request.url);
+  switch (request.method) {
+    case 'POST':
+      if (urlStruct[parsedURL.pathname]) {
+        const res = response;
+        const body = [];
+
+        request.on('error', (err) => {
+          console.dir(err);
+          res.statusCode = 400;
+          res.end();
+        });
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        });
+        request.on('end', () => {
+          const bodyString = Buffer.concat(body).toString();
+          const bodyParams = query.parse(bodyString);
+
+
+          jsonHandler.addUser(request, res, bodyParams);
+        });
+      } else {
+        jsonHandler.notFound(request, response);
+      }
+      break;
+    case 'GET':
+      if (urlStruct[parsedURL.pathname]) {
+        urlStruct[parsedURL.pathname](request, response);
+      } else {
+        jsonHandler.notFound(request, response);
+      }
+      break;
+    case 'HEAD':
+      if (urlStruct[parsedURL.pathname]) {
+        urlStruct[parsedURL.pathname](request, response);
+      } else {
+        jsonHandler.notFoundMeta(request, response);
+      }
+      break;
+    default:
+      jsonHandler.notFound(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
